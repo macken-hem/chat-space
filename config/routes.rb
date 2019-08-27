5692e166c8db3e7c1853dbe46f0330463e85799f
macken-hem <ken.kgaf@gmail.com>
2019-08-27 19:15:52 +0900

    create_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
+  resources :groups, only: [:new, :create, :edit, :update] do
   resources :users, only: [:edit, :update]
+  end
 end
