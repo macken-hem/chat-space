4a6ead2dfe55201cb74c56853c8a691ab42959a6
macken-hem <ken.kgaf@gmail.com>
2019-09-08 19:03:25 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '3.114.183.126', user: 'ec2-user', roles: %w{app db web
+server '3.114.183.126', user: 'ec2-user', roles: %w{app db web}
